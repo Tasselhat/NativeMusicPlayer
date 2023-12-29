6160dd464472264044409ab0e04bb8ff0ca9cc12
Tim Schneider <TIM.B.SCHNEIDER@GMAIL.COM>
2023-12-28 23:03:56 -0800

    idk

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import React from "react";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AudioProvider from "./app/context/AudioProvider";
+import { colors } from "./app/misc/color";
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: colors.APP_BG,
+  },
+};
 
 export default function App() {
   return (
     <AudioProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={MyTheme}>
         <AppNavigator />
       </NavigationContainer>
     </AudioProvider>
--- app/screens/Playlist.js
+++ app/screens/Playlist.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from "react-native";
 
 const Playlist = ({}) => (
-  <View style={styles.container}>
-    <Text>Playlist</Text>
-  </View>
+  <ScrollView contentContainerStyle={styles.container}>
+    <TouchableOpacity style={styles.playlistBanner}>
+      <Text>My Favorites</Text>
+      <Text style={styles.audioCount}>0 songs</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={{ marginTop: 15 }}>
+      <Text style={styles.playlistButton}>+ Add New Playlist</Text>
+    </TouchableOpacity>
+  </ScrollView>
 );
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
+  },
+  playlistBanner: {
+    padding: 5,
+    backgroundColor: color.FONT_MEDIUM,
+    borderRadius: 5,
+  },
+  audioCount: {
+    marginTop: 3,
+    opacity: 0.7,
+    fontSize: 14,
+  },
+  playlistButton: {
+    color: color.ACTIVE_BG,
+    letterSpacing: 1,
+    fontWeight: "bold",
+    fontSize: 14,
+    padding: 5,
   },
 });
 
